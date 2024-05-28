f29f148cedc01d4890c847f3ee74eb466fa94906
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-05-28 09:26:23 -0300

    removed google tag manager

--- functions.php
+++ functions.php
@@ -61,31 +61,6 @@ function removeScriptFromWpRocketCache( $inline_exclusions_list ) {
 add_filter( 'rocket_defer_inline_exclusions', 'removeScriptFromWpRocketCache');
 
 
-function googleTagManagerOnHead(){
-	echo "
-	<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W9B92LV');</script>
-<!-- End Google Tag Manager -->
-";
-}
-add_action("wp_head", "googleTagManagerOnHead");
-
-
-
-function googleTagManagerOnBody(){
-	echo '
-	<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9B92LV"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-';
-}
-add_action('wp_body_open', 'googleTagManagerOnBody');
-
 
 function removePageTitleFromAllPages($return){
 	return false;
